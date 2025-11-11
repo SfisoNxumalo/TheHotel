45975191d0ccb9c00f0de6379323000cc1bae4e0
Sifiso Mawila <Sifiso@nebula.co.za>
2025-11-12 00:41:58 +0200

    Add custom useFetchMessages hook

--- TheHotelFrontend/TheHotel/src/services/messageService.ts
+++ TheHotelFrontend/TheHotel/src/services/messageService.ts
@@ -1,13 +1,34 @@
-import { MessageEndpoint } from "../endpoints/endpoints";
+import { useQuery, UseQueryResult } from "@tanstack/react-query";
+import { getMessageEndpoint, messageEndpoint } from "../endpoints/endpoints";
+import { Message } from "../Interfaces/message";
 import { httpService } from "../utils/httpService";
+import { SendMessage } from "../Interfaces/SendMessage";
 
-export async function getAllMessages(){
-    
-   return await httpService.get(MessageEndpoint)
-      .then((res) => {return res.data;}
-      )
-      .catch((err) => console.error('Failed to fetch messages:', err))
-      .finally(() => {});
+export async function getAllMessages(userId:string){
 
-//    return response;
+   try {
+    const res = await httpService.get(getMessageEndpoint(userId))
+    return res;
+  } catch (err) {
+    console.error('Failed to fetch messages:', err);
+    throw err; // rethrow if you want the caller to handle it
+  }
+}
+
+export const useFetchMessages = (userId:string): UseQueryResult<Message[]> => {
+   return useQuery<Message[]>({
+      queryKey: ["messages", userId],
+      queryFn: async (): Promise<Message[]> => (await getAllMessages(userId)).data,
+      staleTime: 120 * 60 * 1000
+   })
+}
+
+export async function sendMessage(message: SendMessage) {
+  try {
+    const res = await httpService.post<Message>(messageEndpoint, message);
+    return res;
+  } catch (err) {
+    console.error('Failed to send message:', err);
+    throw err; // rethrow if you want the caller to handle it
+  }
 }
