4812eec542cf65216fb040b171ea422cacb13965
Sifiso Mawila <Sifiso@nebula.co.za>
2025-11-16 15:32:54 +0200

    Add new place order service

--- TheHotelFrontend/TheHotel/src/services/roomServiceService.ts
+++ TheHotelFrontend/TheHotel/src/services/roomServiceService.ts
@@ -1,17 +1,16 @@
 import { useQuery, UseQueryResult } from "@tanstack/react-query";
-import { getMenuItemByIdEndpoint, RoomServiceEndpoint } from "../endpoints/endpoints";
+import { getMenuItemByIdEndpoint, PlaceOrderEndpoint, RoomServiceMenuEndpoint } from "../endpoints/endpoints";
 import { httpService } from "../utils/httpService";
 import { Product } from "../Interfaces/products";
+import { Checkout } from "../Interfaces/CartItem";
 
 export async function getAllProducts() {
     
-   return await httpService.get(RoomServiceEndpoint)
+   return await httpService.get(RoomServiceMenuEndpoint)
       .then((res) => {return res.data;}
       )
       .catch((err) => console.error('Failed to fetch products:', err))
       .finally(() => {});
-
-//    return response;
 }
 
 export const useFetchProducts = (): UseQueryResult<Product[]> => {
@@ -26,6 +25,17 @@ export const useFetchProducts = (): UseQueryResult<Product[]> => {
 export async function getMenuItemById(id:string) {
    try{
       const result = await httpService.get(getMenuItemByIdEndpoint(id));
+      return result;
+   }
+   catch(error){
+      console.error('Failed to fetch product:', error)
+      throw error; // rethrow if you want the caller to handle it
+   }
+}
+
+export async function PlaceOrder(CheckoutDetails:Checkout){
+   try{
+      const result = await httpService.post(PlaceOrderEndpoint, CheckoutDetails);
       return result;
    }
    catch(error){
