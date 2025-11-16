d9bf7e2d4960dc2517c83a52bd7fbdfc672c8e06
Sifiso Mawila <Sifiso@nebula.co.za>
2025-11-16 10:36:45 +0200

    Add checkout cart interface

--- TheHotelFrontend/TheHotel/src/Interfaces/CartItem.ts
+++ TheHotelFrontend/TheHotel/src/Interfaces/CartItem.ts
@@ -1,9 +1,13 @@
 export interface CartItem {
   id: string;
   itemName: string;
-  description: string;
   price: number;
   image: string;
   quantity: number;
   note:string;
+}
+
+export interface Checkout{
+  userId:string,
+  items:CartItem[]
 }
