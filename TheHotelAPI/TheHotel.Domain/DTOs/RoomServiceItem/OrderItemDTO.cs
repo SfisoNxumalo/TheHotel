14bc019507a7a09016fd7e828f73d34e4861a56e
Sifiso Mawila <Sifiso@nebula.co.za>
2025-11-17 10:40:55 +0200

    Add new room service order endpoint and service

--- TheHotelAPI/TheHotel.Domain/DTOs/RoomServiceItem/OrderItemDTO.cs
+++ TheHotelAPI/TheHotel.Domain/DTOs/RoomServiceItem/OrderItemDTO.cs
@@ -1,14 +1,15 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace TheHotel.Domain.DTOs.RoomServiceItem
+﻿namespace TheHotel.Domain.DTOs.RoomServiceItem
 {
     public class OrderItemDTO
     {
-        [Required]
-        public required Guid ItemId { get; set; }
+        public required Guid Id { get; set; }
+        public required string ItemName { get; set; } = null!;
 
-        [Required]
-        public int Quantity { get; set; }
+        public required decimal Price { get; set; }
+
+        public required int Quantity { get; set; }
+
+        public required string note { get; set; }
 
 
     }
