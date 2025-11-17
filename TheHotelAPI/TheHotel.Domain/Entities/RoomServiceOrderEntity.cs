fd899d96d3ed40fd1057e443da694630b81b9d5f
Sifiso Mawila <Sifiso@nebula.co.za>
2025-11-17 10:51:33 +0200

    Update DTOs and add missing properties

--- TheHotelAPI/TheHotel.Domain/Entities/RoomServiceOrderEntity.cs
+++ TheHotelAPI/TheHotel.Domain/Entities/RoomServiceOrderEntity.cs
@@ -5,12 +5,14 @@ namespace TheHotel.Domain.Entities
     public class RoomServiceOrderEntity : BaseEntity
     {
         [Required]
-        public required Guid BookingId { get; set; }
+        public required Guid UserId { get; set; }
+
+        public string? Note { get; set; } = "";
 
         [Required]
         public string Status { get; set; } = "Pending";
 
-        public BookingEntity? Booking { get; set; } = null!;
+        public UserEntity? User { get; set; } = null!;
 
         public required ICollection<RoomServiceOrderItemEntity> Items { get; set; } = new List<RoomServiceOrderItemEntity>();
     }
