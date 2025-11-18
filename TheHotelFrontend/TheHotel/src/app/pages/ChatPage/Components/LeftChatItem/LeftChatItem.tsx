1ccf5cba819042e9b9439672f4af1dde3e3bda13
Sifiso Mawila <Sifiso@nebula.co.za>
2025-11-18 04:51:26 +0200

    Add connection hub and snackbar to display order updates

--- TheHotelFrontend/TheHotel/src/app/pages/ChatPage/Components/LeftChatItem/LeftChatItem.tsx
+++ TheHotelFrontend/TheHotel/src/app/pages/ChatPage/Components/LeftChatItem/LeftChatItem.tsx
@@ -1,3 +1,4 @@
+
 import { Message } from '../../../../../Interfaces/message';
 import styles from './LeftItemChat.module.css'
 
@@ -12,10 +13,10 @@ export default function LeftChatItem({message}:LeftChatItemProps){
         <div className={styles.holder}>
             <div className={styles.MessageHolder}>
                 <p>
-                    {message.message}
+                    {message.messageText}
                 </p>
                 <div className={styles.metaData}>
-                    <label>{(String(message.date).substring(11, 16)) }</label>
+                    <label>{(String(message.createdDate).substring(11, 16)) }</label>
                 </div>
                 
             </div>
---
+++ TheHotelFrontend/TheHotel/src/components/Snackbar/ShowSnackBar.tsx
@@ -0,0 +1,43 @@
+import { Button, Snackbar, SnackbarCloseReason } from "@mui/material";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+interface SnackbarProps{
+    open: boolean,
+    setOpen: (open:boolean) => void
+    showButton: boolean,
+    buttonText:string
+    message:string,
+    url: string
+}
+
+export default function ShowCustomSnackbar({open = false, setOpen, message, url, showButton, buttonText}:SnackbarProps)
+{
+    const navigate = useNavigate();
+
+    const handleClose = (
+    event: React.SyntheticEvent | Event,
+    reason?: SnackbarCloseReason,
+  ) => {
+    if (reason === 'clickaway') {
+        console.log('close');
+        
+      return;
+    }
+    setOpen(false)
+  };
+    return(
+        <Snackbar
+            open={open }
+            autoHideDuration={5000}
+            onClose={handleClose}
+            message={message}
+            action={showButton ? (
+                <Button color="secondary" size="small" onClick={()=>{navigate(url)}}>
+                    {buttonText}
+                </Button>
+                ) : null
+            }
+        />
+    )
+}
