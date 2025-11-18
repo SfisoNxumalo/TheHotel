b05e6136161af0d82d2f15063106e69722fdea18
Sifiso Mawila <Sifiso@nebula.co.za>
2025-11-18 14:58:50 +0200

    update message services to implement messageHubs

--- TheHotelAPI/TheHotel.Application/Interfaces/IRealTimeNotifier.cs
+++ TheHotelAPI/TheHotel.Application/Interfaces/IRealTimeNotifier.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TheHotel.Domain.DTOs.MessageDTO;
 using TheHotel.Domain.DTOs.RoomServiceOrderDTO;
 
 namespace TheHotel.Application.Interfaces
 {
     public interface IRealTimeNotifier
     {
-        Task BroadcastMessage(Guid userId, string message);
+        Task BroadcastMessage(Guid userId, FetchMessageDTO message);
 
         Task BroadcastOrderStatusUpdate(Guid userId, UpdateOrderStatus order);
     }
