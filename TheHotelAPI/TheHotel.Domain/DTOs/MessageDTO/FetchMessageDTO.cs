d043f6f2742b93a85fc75a3ec4fc5cfe3d33cc8d
Sifiso Mawila <Sifiso@nebula.co.za>
2025-11-19 15:05:38 +0200

    Add senderId property to DTO and add isDeleted property in entity

--- TheHotelAPI/TheHotel.Domain/DTOs/MessageDTO/FetchMessageDTO.cs
+++ TheHotelAPI/TheHotel.Domain/DTOs/MessageDTO/FetchMessageDTO.cs
@@ -1,17 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TheHotel.Domain.DTOs.MessageDTO
+﻿namespace TheHotel.Domain.DTOs.MessageDTO
 {
     public class FetchMessageDTO
     {
         public Guid Id { get; set; }
         public Guid UserId { get; set; }
-        public Guid senderId { get; set; }
+        public Guid SenderId { get; set; }
 
         public string MessageText { get; set; }
 
