995f29bcd5d49192030078a5a1c7b4a4df91eaf4
Sifiso Mawila <Sifiso@nebula.co.za>
2025-11-22 00:06:17 +0200

    Add AuthModel to pass token and refreshToken to controller

---
+++ TheHotelAPI/TheHotel.Application/Models/AuthModel.cs
@@ -0,0 +1,10 @@
+﻿using TheHotel.Domain.DTOs.UserDTO;
+
+namespace TheHotel.Application.Models
+{
+    public class AuthModel
+    {
+        public UserDetailsDTO userDetails { get; set; }
+        public string RefreshToken { get; set; } 
+    }
+}
