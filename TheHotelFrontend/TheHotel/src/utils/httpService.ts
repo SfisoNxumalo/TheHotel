66682d470b8a6bedeac8c4736b8eebc310906307
Sifiso Mawila <Sifiso@nebula.co.za>
2025-11-24 00:51:59 +0200

    Update axios interceptor to include token

--- TheHotelFrontend/TheHotel/src/utils/httpService.ts
+++ TheHotelFrontend/TheHotel/src/utils/httpService.ts
@@ -1,4 +1,15 @@
 import axios from "axios";
 import { baseURL } from "../endpoints/endpoints";
+import { useAuthStore } from "../stores/authStore";
 
 export const httpService = axios.create({baseURL:baseURL, headers:{}, timeout:10000});
+  
+httpService.interceptors.request.use((config) => {
+    const user = useAuthStore.getState().user;
+
+  if (user?.token) {
+    config.headers.Authorization = `Bearer ${user.token}`;
+  }
+
+  return config;
+});
