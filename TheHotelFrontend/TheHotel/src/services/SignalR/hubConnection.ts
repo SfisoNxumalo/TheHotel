2d589ec1c44c2b6863f17a65d73f91070632e6d5
Sifiso Mawila <Sifiso@nebula.co.za>
2026-01-29 19:25:28 +0200

    Add comment to indicate that connection strings should be in the .env

--- TheHotelFrontend/TheHotel/src/services/SignalR/hubConnection.ts
+++ TheHotelFrontend/TheHotel/src/services/SignalR/hubConnection.ts
@@ -1,6 +1,6 @@
 import * as signalR from "@microsoft/signalr";
 
 export const hubConnection = new signalR.HubConnectionBuilder()
-  .withUrl("https://thehotelapi.azurewebsites.net/hubs/hotel")
+  .withUrl("https://thehotelapi.azurewebsites.net/hubs/hotel")  //Hardcoded for demo purposes only, you should never hardcode this. Preferably store this in the .env
   .withAutomaticReconnect()
   .build();
